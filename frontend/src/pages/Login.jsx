5a7e0bdecd9f42a50d9237e8da7a6563d4719d45
Ganesh Walunj <ganesha.swc@gmail.com>
2025-09-08 21:51:22 +0530

    Fix bug

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -23,7 +23,7 @@ export default function Login() {
     }
 
     try {
-      const res = await fetch("http://localhost:4000/api/login", {
+      const res = await fetch("/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
