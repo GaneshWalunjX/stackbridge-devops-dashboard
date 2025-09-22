50142477feef82fc35e3f1d18ca956f55e70991b
Ganesh Walunj <ganesha.swc@gmail.com>
2025-09-22 18:31:04 +0530

    fixed error

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const app = require('./src/app');
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Backend running on port ${PORT}`);
 });
 
